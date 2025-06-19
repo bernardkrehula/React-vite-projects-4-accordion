4c6b81727098bf7aac093c79145de5c27db4ac23
Bernard Krehula <krehula.bernard456@gmail.com>
2025-06-19 15:54:38 +0200

    Fix showing info on click

--- project-4-accordion/src/App.jsx
+++ project-4-accordion/src/App.jsx
@@ -2,35 +2,35 @@ import { useState } from 'react';
 import questions from './questions'
 import './App.css'
 
-function App() {
- const  [isExpanded, setExpanded] = useState(false); 
-
- const SingleQuestion = (expanded) => {
-  setExpanded(!expanded);
- }
+const SingleQuestion = ({id, title, info}) => {
+  const  [isExpanded, setExpanded] = useState(false); 
+  console.log(title)
+  return(
+    <li className='question' key={id}>
+      <div>
+        <h2>{title}</h2>
+        <button onClick={() => setExpanded(prev => !prev)}>
+          {isExpanded ? '-' : '+'}
+        </button>
+      </div>
+      <h3 style={{ display: isExpanded ? 'block' : 'none' }}>{info}</h3>
+    </li>
+  )
+}
 //Napravi komponentu singleQuestion 
 //u tu komponentu napravi state isExpanded 
 //Na click dugmeta stavi da je taj isExpanded obrnuto od onoga sto je bio
-
+function App() {
   return (
     <>
       <div className='main'>
         <h1 className='title'>Questions</h1>
-        {questions.map(question => {
-          const { id, title, info } = question;
-          return (
-          <>
-{/*           <SingleQuestion/>
- */}        <li key={id} className='question'>
-            <div>
-              <h2>{title}</h2>
-              <button onClick={() => SingleQuestion(isExpanded)}>+</button>
-            </div>
-            <h3 style={{display: isExpanded ? 'block' : 'none'}}>{info}</h3>
-          </li>
-          </>
-          )
-        })}
+          {questions.map(question => {
+            const { id, title, info } = question;
+            return (
+              <SingleQuestion id={id} title={title} info={info}/>
+            )
+          })}
       </div>
     </>
   )
